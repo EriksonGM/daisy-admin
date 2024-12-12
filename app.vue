80dd446898f823d7fbda34c8488b802112bc40d4
EriksonGM <erik.melgarejo@hotmail.com>
2024-12-12 10:54:06 +0100

    tailwind and daisy ready

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtRouteAnnouncer/>
+  <NuxtLoadingIndicator/>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
+
 </template>
+<script setup lang="ts">
+</script>
